9e0bc9e1f6d3968a5bcbbd4f9f1fb26e597a36a6
Lucas Krul <l3k.scorp@gmail.com>
2019-05-26 13:56:33 -0300

    first commit

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-flex class="pr-3 pb-3" xs12 md6 lg4>
+        <v-card class="green darken-3 white--text">
+            <v-card-title class="headline">
+                <strong>{{ stock.name }} <small>(preço: {{ stock.price }})</small></strong>
+            </v-card-title>
+        </v-card>
+
+        <v-card>
+            <v-container fill-height>
+                <v-text-field label="Quantidade" type="number"></v-text-field>
+                <v-btn class="green darken-3 white--text">Comprar</v-btn>
+            </v-container>
+        </v-card>
+    </v-flex>
+</template>
+
+<script>
+export default {
+    props: {
+        stock: {
+            type: Object,
+            required: true 
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
